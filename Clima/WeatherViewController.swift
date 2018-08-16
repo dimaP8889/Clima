65fed06fe0772770c0f9e38eb91dfc56894a003b
dima <dima.squash123@gmail.com>
2018-08-16 19:39:41 +0300

    finnish app

---
+++ Clima/WeatherViewController.swift
@@ -0,0 +1,193 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Angela Yu on 23/08/2015.
+//  Copyright (c) 2015 London App Brewery. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+import Alamofire
+import SwiftyJSON
+
+class WeatherViewController: UIViewController, CLLocationManagerDelegate, ChangeCityDelegate{
+    
+    //Constants
+    let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
+    let APP_ID = "e72ca729af228beabd5d20e3b7749713"
+    
+    let locationManager = CLLocationManager()
+    let weatherDataModule = WeatherDataModel()
+    //TODO: Declare instance variables here
+    
+
+    
+    //Pre-linked IBOutlets
+    @IBOutlet weak var weatherIcon: UIImageView!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    
+    var temperature : Int = 0
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        //TODO:Set up the location manager here.
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
+        //updateUIWeatherData()
+    }
+    
+    
+    
+    //MARK: - Networking
+    /***************************************************************/
+    func getWeatherData(url: String, parameters: [String : String]) {
+        
+        Alamofire.request(url, method : .get, parameters: parameters).responseJSON {
+            response in
+            if response.result.isSuccess {
+                print("Succes! Got the weather data")
+                
+                let weatherJson : JSON = JSON(response.result.value!)
+                
+                print(weatherJson)
+                
+                self.updateWeatherData(json: weatherJson)
+            }
+            else {
+                print("Error \(String(describing: response.result.error))")
+                self.cityLabel.text = "Connection Issues"
+            }
+        }
+    }
+    //Write the getWeatherData method here:
+    
+    
+    
+    
+    
+    
+    //MARK: - JSON Parsing
+    /***************************************************************/
+   
+    
+    //Write the updateWeatherData method here:
+    func updateWeatherData(json : JSON) {
+        
+        if let tempResult = json["main"]["temp"].double {
+        weatherDataModule.temperature = Int(tempResult - 273.15)
+        
+        weatherDataModule.city = json["name"].stringValue
+        
+        weatherDataModule.condition = json["weather"][0]["id"].intValue
+        
+        weatherDataModule.weatherIconName = weatherDataModule.updateWeatherIcon(condition: weatherDataModule.condition!)
+            
+        updateUIWeatherData()
+        }
+        else {
+            cityLabel.text = "Weather Unavaible"
+        }
+    }
+
+    
+    
+    
+    //MARK: - UI Updates
+    /***************************************************************/
+    
+    func updateUIWeatherData() {
+        
+        temperatureLabel.text = String(weatherDataModule.temperature!) + "°"
+        temperature = Int(weatherDataModule.temperature!)
+        cityLabel.text = weatherDataModule.city!
+        weatherIcon.image = UIImage(named: weatherDataModule.weatherIconName!)        
+    }
+    //Write the updateUIWithWeatherData method here:
+    
+    
+    
+    
+    
+    
+    //MARK: - Location Manager Delegate Methods
+    /***************************************************************/
+    
+    
+    //Write the didUpdateLocations method here:
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let location = locations[locations.count - 1]
+        if location.horizontalAccuracy > 0 {
+            locationManager.startUpdatingLocation()
+            locationManager.delegate = nil
+            
+            print("longitude = \(location.coordinate.longitude), latitude = \(location.coordinate.latitude)")
+            
+            let latitude = String(location.coordinate.latitude)
+            let longitude = String(location.coordinate.longitude)
+            
+            let params : [String : String] = ["lat" : latitude, "lon" : longitude, "appid" : APP_ID]
+            
+            getWeatherData(url: WEATHER_URL, parameters: params)
+        }
+    }
+    
+    //Write the didFailWithError method here:
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        
+        print(error)
+        cityLabel.text = "Location Unavailable"
+    }
+    
+    
+
+    
+    //MARK: - Change City Delegate methods
+    /***************************************************************/
+    
+    
+    //Write the userEnteredANewCityName Delegate method here:
+    func userEnteredANewCityName(city: String) {
+        let params : [String : String] = ["q" : city, "appid" : APP_ID]
+        
+        getWeatherData(url: WEATHER_URL, parameters: params)
+    }
+
+    
+    //Write the PrepareForSegue Method here
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "changeCityName" {
+            
+            let destinationVC = segue.destination as! ChangeCityViewController
+            
+            destinationVC.delegate = self
+        }
+    }
+    
+    //Switch Button
+    
+    @IBAction func switchButton(_ sender: UISwitch) {
+        if (sender.isOn == true) {
+            temperatureLabel.text! = String(temperature) + "°"
+        }
+        else if (sender.isOn == false) {
+            let tempFar = temperature * 9/5 + 32
+            temperatureLabel.text! = String(tempFar) + "°"
+        }
+    }
+    
+    
+    
+}
+
+
---
+++ Podfile
@@ -0,0 +1,14 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '11.4'
+
+target 'Clima' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Clima
+    pod 'Alamofire'
+    pod 'SwiftyJSON'
+    pod 'SVProgressHUD'
+    pod 'OAuth2'
+
+end
